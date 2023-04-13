26d2c98ec622b12e438ebeb7e5796c09d9691237
Cody Bromley <codybrom@users.noreply.github.com>
2023-04-13 13:20:16 -0500

    refactor activate function

--- src/extension.ts
+++ src/extension.ts
@@ -12,29 +12,8 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.window.showErrorMessage('Please open a workspace to use this extension.');
         return;
       }
-
       const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-      const gptContext = await createGPTFriendlyContext(workspacePath);
-
-      const gptContextDocument = await vscode.workspace.openTextDocument({
-        content: gptContext,
-        language: 'plaintext',
-      });
-
-      await vscode.env.clipboard.writeText(gptContext);
-      await vscode.window.showTextDocument(gptContextDocument, vscode.ViewColumn.One);
-      vscode.window.showInformationMessage('GPT-friendly context copied to clipboard.');
-
-      const tokenCount = estimateTokenCount(gptContext);
-      if (tokenCount > 8000) {
-        vscode.window.showWarningMessage(
-          `The generated context is approximately ${tokenCount} tokens, which is greater than 8000 tokens.`
-        );
-      } else {
-        vscode.window.showInformationMessage(
-          `The generated context is approximately ${tokenCount} tokens.`
-        );
-      }
+      await handleCommand(workspacePath);
     }
   );
 
@@ -45,43 +24,46 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.window.showErrorMessage('Please open a workspace and a file to use this extension.');
         return;
       }
-
-      const config = vscode.workspace.getConfiguration('gpt-context-generator');
-      const detectedFileExtensions = config.get('detectedFileExtensions') as string[];
-      const outputMethod = config.get('outputMethod') as string;
-
       const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
       const openFilePath = vscode.window.activeTextEditor.document.uri.fsPath;
-      const gptContext = await createGPTFriendlyContextForOpenFile(workspacePath, openFilePath);
-
-      if (outputMethod === 'newWindow') {
-        const gptContextDocument = await vscode.workspace.openTextDocument({
-          content: gptContext,
-          language: 'plaintext',
-        });
-
-        await vscode.window.showTextDocument(gptContextDocument, vscode.ViewColumn.One);
-      } else if (outputMethod === 'clipboard') {
-        await vscode.env.clipboard.writeText(gptContext);
-        vscode.window.showInformationMessage('GPT-friendly context copied to clipboard.');
-      }
-
-      const tokenCount = estimateTokenCount(gptContext);
-      if (tokenCount > 8000) {
-        vscode.window.showWarningMessage(
-          `GPT Context: ${tokenCount} tokens, which is greater than 8000 tokens.`
-        );
-      } else {
-        vscode.window.showInformationMessage(
-          `The generated context is approximately ${tokenCount} tokens.`
-        );
-      }
+      await handleCommand(workspacePath, openFilePath);
     }
   );
 
   context.subscriptions.push(disposableOpenFile);
-
   context.subscriptions.push(disposable);
+}
+
+async function handleCommand(workspacePath: string, openFilePath?: string) {
+  const config = vscode.workspace.getConfiguration('gpt-context-generator');
+  const outputMethod = config.get('outputMethod') as string;
+
+  const gptContext = openFilePath
+    ? await createGPTFriendlyContextForOpenFile(workspacePath, openFilePath)
+    : await createGPTFriendlyContext(workspacePath);
+
+  if (outputMethod === 'newWindow') {
+    const gptContextDocument = await vscode.workspace.openTextDocument({
+      content: gptContext,
+      language: 'plaintext',
+    });
+
+    await vscode.window.showTextDocument(gptContextDocument, vscode.ViewColumn.One);
+  } else if (outputMethod === 'clipboard') {
+    await vscode.env.clipboard.writeText(gptContext);
+    vscode.window.showInformationMessage('GPT-friendly context copied to clipboard.');
+  }
+
+  const tokenCount = estimateTokenCount(gptContext);
+  if (tokenCount > 8000) {
+    vscode.window.showWarningMessage(
+      `The generated context is approximately ${tokenCount} tokens, which is greater than 8000 tokens.`
+    );
+  } else {
+    vscode.window.showInformationMessage(
+      `The generated context is approximately ${tokenCount} tokens.`
+    );
+  }
 }
 
 async function createGPTFriendlyContext(workspacePath: string): Promise<string> {
